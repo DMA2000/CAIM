e9cfd0d6762328b383b50016577258a4350cb95f
IDBIDO <hechen185@gmail.com>
2022-09-11 12:00:48 +0200

    get frequency

--- LAB1/borrar_basura.py
+++ LAB1/borrar_basura.py
@@ -1,3 +1,18 @@
 # esto
 # Prueba de push
-open ()
+
+def getFrequency (line):
+    frequency = line[0];
+    for i in range(1, len(line)):
+        if line[i] != ',':
+            frequency = frequency + line[i]
+        else:
+            break;
+    return int(frequency)
+
+
+
+news = open ("news_alpha.txt")
+lineas = news.readlines()
+for i in range (0, len(lineas)-1):
+    print(1)
