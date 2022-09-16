059323ddd9110939ca0d529d723c82d3b8369dbb
IDBIDO <hechen185@gmail.com>
2022-09-16 18:32:28 +0200

    jj

--- LAB1/make_graph.py
+++ LAB1/make_graph.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 def getFrequency (line):
     frequency = line[0];
     for i in range(1, len(line)):
-        frequency = frequency + line[i]
+        if line[i] != ' ':
+            frequency = frequency + line[i]
+        else:
+            break
     return int(frequency)
+
+
+def zipFunction(rank, a, b, c):
+	return c/((rank+b)**a)
 
 news = open ("info_novels.txt")
 lineas = news.readlines()
@@ -16,7 +24,7 @@ array = []
 count = 0
 for l in range (0, total):
     #if (getFrequency(lineas[l]) > 1000):
-    if count < 500:
+    #if count < 500:
         array.append( getFrequency(lineas[l]) )
         count += 1
 
@@ -25,6 +33,13 @@ for l in range (0, total):
 #for l in range(0, total):
 #    f.write(str(array[l]))
 #    f.write('\n')
+
+xList = [(x+1) for x in range(len(array))]
+popt, pcov = curve_fit(zipFunction, xList, array, bounds=([0.9, -100000.0, -100000.0],[1.8, 100000.0, 1000000.0]))
+
+print(popt)
+
+plt.plot(xList, zipFunction(xList, *popt), 'g--', label='fit-with-bounds')
 
 plt.plot(array)
 plt.yscale('log')
