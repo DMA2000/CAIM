c52abea91a199e0f86cb11fa4db4871e8d23b580
IDBIDO <hechen185@gmail.com>
2022-10-01 11:19:56 +0200

    ddd

--- LAB2/Make_Boxplot.py
+++ LAB2/Make_Boxplot.py
@@ -1,5 +1,8 @@
-import seaborn as sns
-import pandas as pd
+#import seaborn as sns
+#import pandas as pd
+
+import matplotlib.pyplot as plt
+import numpy as np
 array = []
 
 results = open ("test.txt")
@@ -9,7 +12,10 @@ total = len(lineas)
 for value in lineas:
     array.append(float(value))
 
-d = {'Valores': array}
-df = pd.DataFrame(data=d)
+#d = {'Valores': array}
+#df = pd.DataFrame(data=d)
 #print(type(array[0]))
-sns.boxplot(x = df['Valores']).imshow()
+#sns.boxplot(x = df['Valores']).imshow()
+
+plt.boxplot(array)
+plt.show()
