76e197eb33cbf6eab67fd2f4332cf5b22c6514ca
IDBIDO <hechen185@gmail.com>
2022-11-06 10:14:31 +0100

    kjk

--- LAB3/Rocchio.py
+++ LAB3/Rocchio.py
@@ -74,7 +74,7 @@ def toTFIDF(client, index, file_id):
         idfi = np.log2((dcount/df))            # idfi es logaritmo base 2 de numero de documentos entre 
                                                # numero de de documentos que contiene el termino
         tfidfw[t] = (tfdi * idfi) # añadimos el resultado a la lista normalizandolo respecto al tamaño del documento
-    return (tfidfw)
+    return normalize(tfidfw)
 
 if __name__ == '__main__':
     #Parsear la entrada
@@ -91,69 +91,78 @@ if __name__ == '__main__':
     nhits = args.nhits
 
 
-    nrounds = 500
+    nrounds = 5
     k = args.nhits
-    alpha = 1.0
-    beta = 0.8
-    R = 10
+    alpha = 1
+    beta = 0
+    R = 6
 
+    datos = []
     try:
         client = Elasticsearch()
         s = Search(using=client, index=index)
 
         if query is not None:
-            #Obtener la query
-            q = Q('query_string',query=query[0])
-            for i in range(1, len(query)):
-                q &= Q('query_string',query=query[i])
+            for i in range(0,nrounds):
+                #Obtener la query
+                q = Q('query_string',query=query[0])
+                for i in range(1, len(query)):
+                    q |= Q('query_string',query=query[i])
 
-            s = s.query(q)
-            response = s[0:nhits].execute()
+                s = s.query(q)
+                #print(query)
+                response = s[0:nhits].execute()
+                # computar palabra-peso
 
-            # computar palabra-peso
-            palabra_peso = {}
-            for q in query:
-                if '^' in q:
-                    aux = q.split('^')
-                    palabra_peso[aux[0]] = float(aux[1])
-                else:
-                    aux = q 
-                    palabra_peso[aux[0]] = 1.0
+                palabra_peso = {}
+                for q in query:
+                    if '^' in q:
+                        aux = q.split('^')
+                        palabra_peso[aux[0]] = float(aux[1])
+                    else:
+                        aux = q 
+                        palabra_peso[aux[0]] = 1.0
+                    
+                #print(palabra_peso);
+                # Calculo Rocchio Rule
+                # Calculo del TFIDF de cada documento
+                sumDocs = {}
+                for r in response:
+                    file_tw = toTFIDF(client, index, r.meta.id) #computo tf-idf de cada documento
+                    sumDocs = {t: sumDocs.get(t,0) + file_tw.get(t,0) for t in set(file_tw) | set(sumDocs)} #suma de valores de los docs
+
+                sumDocs = {t: beta*sumDocs.get(t,0)/nhits for t in set(sumDocs)} #beta * vector de documents / K
+                oldQuery = {t: alpha*palabra_peso.get(t,0) for t in set(palabra_peso)} #alpha * query
+                newQuery = {t: sumDocs.get(t,0) + oldQuery.get(t,0) for t in set(oldQuery)|set(sumDocs)} #newquery = sumDocs + oldquery
+
+                #
+                newQuery = sorted(newQuery.items(), key=operator.itemgetter(1), reverse=True)
+                newQuery = newQuery[:R]
+                # Cálculo token-peso de la query
+                query = []
+                for (term, value) in newQuery:
+                    query.append(term + '^' + str(value))
+
                 
-            print(palabra_peso);
-            # Calculo Rocchio Rule
-            # Calculo del TFIDF de cada documento
-            sumDocs = {}
-            for r in response:
-                file_tw = toTFIDF(client, index, r.meta.id) #computo tf-idf de cada documento
-                sumDocs = {t: sumDocs.get(t,0) + file_tw.get(t,0) for t in set(file_tw) | set(sumDocs)} #suma de valores de los docs
-
-            sumDocs = {t: beta*sumDocs.get(t,0)/nhits for t in set(sumDocs)} #beta * vector de documents / K
-            oldQuery = {t: alpha*palabra_peso.get(t,0) for t in set(palabra_peso)} #alpha * query
-            newQuery = {t: sumDocs.get(t,0) + oldQuery.get(t,0) for t in set(oldQuery)|set(sumDocs)} #newquery = sumDocs + oldquery
-
-            #
-            newQuery = sorted(newQuery.items(), key=operator.itemgetter(1), reverse=True)
-            newQuery = newQuery[:R]
-            # Cálculo token-peso de la query
-            query = []
-            for (term, value) in newQuery:
-                query.append(term + '^' + str(value))
-
-            print(query)
+                #print(query)
+                # imprimir resultados
+                #response = s[0:nhits].execute()
+                
+                for r in response:  # only returns a specific number of results
+                    datos.append(r.meta.score)
+                    print(f'ID= {r.meta.id} SCORE={r.meta.score}')
+                    print(f'PATH= {r.path}')
+                    print(f'TEXT: {r.text[:50]}')
+                    print('-----------------------------------------------------------------')
+                print (f"{response.hits.total['value']} Documents")
             
-            # imprimir resultados
-            response = s[0:nhits].execute()
-            for r in response:  # only returns a specific number of results
-                print(f'ID= {r.meta.id} SCORE={r.meta.score}')
-                print(f'PATH= {r.path}')
-                print(f'TEXT: {r.text[:50]}')
-                print('-----------------------------------------------------------------')
+
         else:
             print('No query parameters passed')
 
         print (f"{response.hits.total['value']} Documents")
-
+        print(query)
+        print(datos)
     except NotFoundError:
         print(f'Index {index} does not exists')
 
