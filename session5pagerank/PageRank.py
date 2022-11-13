70f6b934b0881d959dc2c71f905618a99ec01665
IDBIDO <hechen185@gmail.com>
2022-11-13 12:41:41 +0100

    sss

--- session5pagerank/PageRank.py
+++ session5pagerank/PageRank.py
@@ -6,29 +6,42 @@ import sys
 
 class Edge:
     def __init__ (self, origin=None):
-        self.origin = origin 
-        self.weight = 1 # write appropriate value
+        self.origin = origin
+        self.weight = 1     # inicialmente con peso 1
 
     def __repr__(self):
         return "edge: {0} {1}".format(self.origin, self.weight)
-        
-    ## write rest of code that you need for this class
+
 
 class Airport:
-    def __init__ (self, iden=None, name=None):
-        self.code = iden    # codigo IATA
-        self.name = name    
-        self.routes = []        # routes: lista de edge; un edge es un aeropuerto con este como destimo: sristas de entrada
-        self.routeHash = dict()     
-        self.outweight = 0.0   # outdegree de este aeropuerto, inicialemnte a 0
+    def __init__ (self, iden=None, name=None, index=None):
+        self.code = iden
+        self.name = name
+        self.routeHash = dict()
+        self.outweight = 0.0
+        self.index = index
 
     def __repr__(self):
-        return f"{self.code}\t{self.pageIndex}\t{self.name}"
+        return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
 
-edgeList = [] # list of Edge
-edgeHash = dict() # hash of edge to ease the match
+    def addIncomingEdge(self, incomingAirport):
+        if not incomingAirport in self.routeHash:       # si la arista no esta en su lista ruta, lo anadimos
+            e = Edge(incomingAirport)
+            self.routeHash[incomingAirport] = e
+        else:                                           # si esta en lista ruta, incrementamos el peso
+            e = self.routeHash[incomingAirport]
+            e.weight += 1
+
+        airportHash[e.origin].outweight += 1
+
+
+
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
+PR = []
+
+L = 0.85
+diff = 10**(-10)     # diferencia aceptable entre P y Q
 
 def readAirports(fd):
     print("Reading Airport file from {0}".format(fd))
@@ -42,6 +55,7 @@ def readAirports(fd):
                 raise Exception('not an IATA code')
             a.name=temp[1][1:-1] + ", " + temp[3][1:-1]
             a.code=temp[4][1:-1]
+            a.index = cont
         except Exception as inst:
             pass
         else:
@@ -49,9 +63,9 @@ def readAirports(fd):
             airportList.append(a)
             airportHash[a.code] = a
     airportsTxt.close()
-    print(f"There were {cont} Airports with IATA code")
+    print("There were {0} Airports with IATA code".format(cont))
 
-# escribe cada ruta a los aeropuertos
+
 def readRoutes(fd):
     print("Reading Routes file from {0}".format(fd))
     routesTxt = open(fd, "r");
@@ -64,7 +78,7 @@ def readRoutes(fd):
             originCode = temp[2]
             destCode = temp[4]
             if destCode in airportHash and originCode in airportHash:
-                # for an edge (i.j), to compute pagerank we are only interested in which are the incoming edges for an airport
+                # add edge for destination airport
                 destAirport = airportHash[destCode]
                 destAirport.addIncomingEdge(originCode)
             else:
@@ -77,13 +91,88 @@ def readRoutes(fd):
     routesTxt.close()
     print("There were {0} Edges with both IATA code".format(cont))
 
-            
+
+def conectarNodosDosEnDos(discAirp):        # conecta una lista de nodos dos a dos
+    i = 0;
+    while(i < len(discAirp)):
+        discAirp[i].addIncomingEdge(discAirp[i+1].code)
+        discAirp[i+1].addIncomingEdge(discAirp[i].code)
+        i += 2
+
+
 
 def computePageRanks():
-    # write your code
+    n = len(airportHash)
+    P = [1/n]*n
+    # Disconnected nodes PR calculation
+    discAirp = (list(filter(lambda a: a.outweight == 0.0, airportList)))
+    print(len(discAirp))
+
+
+    if len(discAirp)%2 == 0:
+        conectarNodosDosEnDos(discAirp)
+
+    else:
+
+
+        print(discAirp[0].index)
+        discAirp[0].addIncomingEdge(discAirp[1].code)
+        discAirp[1].addIncomingEdge(discAirp[2].code)
+        discAirp[2].addIncomingEdge(discAirp[1].code)
+        discAirp.remove(discAirp[0])
+        discAirp.remove(discAirp[1])
+        discAirp.remove(discAirp[2])
+        conectarNodosDosEnDos(discAirp)
+
+
+    stop = False
+    it = 0
+    while (not stop):
+        Q = [0.0]*n
+        for i in range(n):
+            a = airportList[i]
+            sumPR = 0
+            for k,v in a.routeHash.items():
+                sumPR += P[airportHash[k].index] * v.weight / airportHash[k].outweight
+            Q[i] = L * sumPR + (1-L)/n 
+        stop = checkDifference(diff, P, Q)
+        P = Q
+
+        print("sum PR (iter", it, "):" , sum(i for i in P))    # print sum of P
+        it += 1
+
+    global PR
+    PR = P.copy()
+
+    return it
+
+def checkDifference(diff, P, Q):
+    
+    for x, y in zip(P,Q):
+        if (abs(x-y) > diff):
+            return False
+    return True
 
 def outputPageRanks():
-    # write your code
+    L = []
+    i = 0
+    for k in airportHash:
+        a = airportHash[k]
+        x = (a.name, PR[i])
+        L.append(x)
+        i += 1
+    L.sort(key = lambda x: x[1], reverse = True)
+
+    s = ""
+    s += " (Airport_Name, Location : PageRank) \n"
+    for (x,y) in L:
+        s += ("(%s : %s)\n"%(x, y))
+
+    f = open("output.txt", "w")
+    f.write(s)
+    f.close()
+
+
 
 def main(argv=None):
     readAirports("airports.txt")
@@ -92,7 +181,7 @@ def main(argv=None):
     iterations = computePageRanks()
     time2 = time.time()
     outputPageRanks()
-    print("#Iterations:", iterations)
+    print("# Iterations:", iterations, ", with", diff, "Difference between iterations")
     print("Time of computePageRanks():", time2-time1)
 
 
